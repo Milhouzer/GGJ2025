9734d444bc39d8c661fc9a3d3893d1974e4d1ac9
ABaccot <alexandre.baccot@gmail.com>
2025-01-25 19:19:27 +0100

    [Add] movement restriction for eraser

--- Assets/Scripts/CaptainNemo/Octopus/InkSplatEraser.cs
+++ Assets/Scripts/CaptainNemo/Octopus/InkSplatEraser.cs
@@ -1,13 +1,20 @@
+using System.Numerics;
 using CaptainNemo.Controls;
-using Unity.VisualScripting;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class InkSplatEraser : ControlHandler
 {
     [SerializeField] private float eraserRotationSpeed = 25f;
+    [SerializeField] private float eraserRotationMaxAngle = 77f;
+    [SerializeField] private Transform eraserBody = default;
 
     private Quaternion eraserInitialRotation;
     private Quaternion eraserMaxRotation;
+
+    private bool isGoingRight = true;
 
     protected override void OnHandle()
     {
@@ -21,8 +28,45 @@ public class InkSplatEraser : ControlHandler
         if (mainCam != null)
         {
             Vector2 mousePos = UnityEngine.Input.mousePosition;
-            Vector3 toLookAt = new Vector3(UnityEngine.Input.mousePosition.x, UnityEngine.Input.mousePosition.y, -mainCam.transform.position.z);
-            transform.LookAt(mainCam.ScreenToWorldPoint(toLookAt));
+            Vector3 toLookAt = new Vector3(mousePos.x, mousePos.y, -mainCam.transform.position.z);
+            Vector3 toMouse = toLookAt - transform.position;
+            Vector3 computedPos = mainCam.ScreenToWorldPoint(toMouse);
+
+            float mouseAngle = Vector3.SignedAngle(Vector3.down, computedPos - transform.position, Vector3.forward);
+            float eraserAngle = Vector3.SignedAngle(Vector3.down, eraserBody.up, Vector3.forward);
+
+            if (Mathf.Abs(mouseAngle) - Mathf.Abs(eraserAngle) > 90)
+            {
+                Release();
+            }
+            
+            if (isGoingRight)
+            {
+                if (mouseAngle > eraserAngle)
+                {
+                    return;
+                }
+
+                if (mouseAngle < -eraserRotationMaxAngle)
+                {
+                    isGoingRight = false;
+                    return;
+                }
+                
+            }else
+            {
+                if (mouseAngle < eraserAngle)
+                {
+                    return;
+                }
+
+                if (mouseAngle > eraserRotationMaxAngle)
+                {
+                    isGoingRight = true;
+                    return;
+                }
+            }
+            transform.LookAt(computedPos);
         }
     }
 
