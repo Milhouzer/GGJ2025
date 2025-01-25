672972473db2bf2c0d38b4106df6e45d659053d8
AdrienLMR <adrienlemaire.pro32@gmail.com>
2025-01-25 12:19:25 +0100

    Update bubble movement

--- Assets/Scripts/CaptainNemo/BubbleManager.cs
+++ Assets/Scripts/CaptainNemo/BubbleManager.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class BubbleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //For test
+    public Bubble bubble = default;
+
+    public AnimationCurve minHeatSpeedCurve = default;
+    public AnimationCurve maxHeatSpeedCurve = default;
+
+    public AnimationCurve mouthStrengthByTime = default;
+    //
+
+    public static float fieldRay { get; } = 4.5f;
+
+    private void Awake()
     {
-        
+        bubble.CurrentMouthStrength = mouthStrengthByTime.Evaluate(Time.time / 10f);
+        bubble.CurrentRandomSpeed = 1f;// = new Vector2(minHeatSpeedCurve.Evaluate(Time.time / 10f), maxHeatSpeedCurve.Evaluate(Time.time / 10f)) * 10;
+        bubble.CurrentRandomRotationRange = new Vector2(-30, 30);// = Random.Range(minHeatSpeedCurve.Evaluate(Time.time / 10f), maxHeatSpeedCurve.Evaluate(Time.time / 10f)) * 10;
+        bubble.CurrentTimeBetweenAddForce = 0.05f;// minHeatSpeedCurve.Evaluate(Time.time / 10f);
+        bubble.Init(5);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //bubble.CurrentMouthStrength = mouthStrengthByTime.Evaluate(Time.time / 10f);
+        //bubble.CurrentRandomSpeedRange = new Vector2(minHeatSpeedCurve.Evaluate(Time.time / 10f), maxHeatSpeedCurve.Evaluate(Time.time / 10f)) * 10;
+        //bubble.CurrentRandomRotationSpeed = Random.Range(minHeatSpeedCurve.Evaluate(Time.time / 10f), maxHeatSpeedCurve.Evaluate(Time.time / 10f)) * 10;
+        //bubble.CurrentTimeBetweenAddForce = minHeatSpeedCurve.Evaluate(Time.time / 10f);
     }
+    //
 }
