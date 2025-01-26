d598d32193ef178ef450d4746ef096c01b85845a
ClMartin2 <clmartinpro1@gmail.com>
2025-01-26 15:16:54 +0100

    [ADD] Eyes follow mouse

---
+++ Assets/Scripts/CaptainNemo/Juiciness/EyeFollowMouse.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EyeFollowMouse : MonoBehaviour
+{
+	[SerializeField] private float force = 0.01f;
+	[SerializeField] private Vector2 clampDistance = Vector2.zero;
+	[SerializeField] private Vector3 offsset;
+
+	private Vector3 startPosition = Vector3.zero;
+
+	private void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	private void Update()
+	{
+		Vector3 screenMousePosition = new Vector3(UnityEngine.Input.mousePosition.x, UnityEngine.Input.mousePosition.y,
+			Camera.main.transform.position.z * -1);
+		Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(screenMousePosition);
+
+		Debug.Log("mouse Position world " + worldMousePosition);
+
+		Vector3 direction = worldMousePosition - startPosition;
+		float distance =  Mathf.Clamp(direction.magnitude,clampDistance.x,clampDistance.y);
+		direction = direction.normalized;
+
+		Debug.DrawRay(startPosition, direction * distance * force, Color.red,100f);
+
+		transform.position = startPosition + offsset + direction * distance * force;
+	}
+}
