5acf69c637e183ce3e535edbac1dc23930c85289
ClMartin2 <clmartinpro1@gmail.com>
2025-01-26 10:55:37 +0100

    [ADD] shake tentacule

--- Assets/Scripts/CaptainNemo/SeaCreatures/Tentacle.cs
+++ Assets/Scripts/CaptainNemo/SeaCreatures/Tentacle.cs
@@ -1,15 +1,16 @@
 using CaptainNemo.Controls;
+using DG.Tweening;
 
 namespace CaptainNemo.SeaCreature
 {
     public delegate void OnTentacleDeathEventHandler(Tentacle sender);
 
     public class Tentacle : ControlHandler
-    {
+    {   
         private int _hp = 1;
         private float _hitShakeStrength = 1;
         public event OnTentacleDeathEventHandler OnTentacleDeath;
-    
+        
 
         public void Initialize(int hp, float hitShakeStrength)
         {
@@ -26,7 +27,7 @@ namespace CaptainNemo.SeaCreature
         protected override void OnHandle()
         {
             _hp--;
-        
+
             if (_hp <= 0)
             {
                 OnTentacleDeath?.Invoke(this);
